b63e6a075c6eeba4c2c048568210d17844e3b00c
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-06-15 22:28:33 -0300

    Aula 02

---
+++ main.js
@@ -0,0 +1,16 @@
+function onScroll() {
+  const nav = document.querySelector('#navigation')
+  if (scrollY > 0) {
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
